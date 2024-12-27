74c3d1b8a231306b94662fbf4130d3e0cd56c6e7
Michael Bledsoe <michael.bledsoe@gmail.com>
2024-12-27 09:24:47 -0500

    Updated dimensions values

--- blockattack.js
+++ blockattack.js
@@ -1,22 +1,23 @@
 
 class BlockAttack {
+    width = 800;
     height = 800;
-    width = 600;
+    blockSize = 80;
 
     constructor(canvas) {
         this.canvas = canvas;        
     }
 
     run() {
-        this.canvas.setAttribute('width', this.height);
-        this.canvas.setAttribute('height', this.width);
+        this.canvas.setAttribute('width', this.width);
+        this.canvas.setAttribute('height', this.height);
 
         const ctx = this.canvas.getContext("2d");
 
         ctx.strokeStyle = "#000000";
-        ctx.strokeRect(0, 0, this.height, this.width);
+        ctx.strokeRect(0, 0, this.width, this.height);
 
         ctx.fillStyle = "#FF0000";
-        ctx.fillRect(0, 0, 200, 200);
+        ctx.fillRect(0, 0, this.blockSize, this.blockSize);
     }
 }
